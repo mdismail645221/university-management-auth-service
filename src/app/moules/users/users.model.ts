c5c5c5520333c29f34c8af68f73ebdc8f3440d4c
Ismail Hossain <you@example.com>
2023-06-07 23:18:52 +0600

    [DONE] setup lint, prettier, husky, lint-staged

---
+++ src/app/moules/users/users.model.ts
@@ -0,0 +1,10 @@
+// import { Schema, model } from 'mongoose'
+// import { IUser } from './users.interface'
+
+// export const userSchema = new Schema<IUser>({
+//   id: {
+//     type: string,
+//     require: true,
+//     unique: true,
+//   },
+// })
--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,18 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
-
-
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
 async function bootstrap() {
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log(`Database is connected successfully`)
 
-    try {
-        await mongoose.connect(config.database_url as string);
-        console.log(`Database is connected successfully`)
-
-        app.listen(config.path, () => {
-            console.log(`Application app listening on port ${config.path}`)
-          })
-
-
-    } catch (error) {
-        console.log(`Server is error now`, error)
-    }
-
+    app.listen(config.path, () => {
+      console.log(`Application app listening on port ${config.path}`)
+    })
+  } catch (error) {
+    console.log(`Server is error now`, error)
   }
+}
 
-
-  bootstrap()
+bootstrap()
