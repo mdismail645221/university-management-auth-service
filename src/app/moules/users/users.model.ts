2cfffe16b596ab862b948f83482e7fd0ab833fc7
Ismail Hossain <you@example.com>
2023-06-09 09:52:45 +0600

    [DONE] setup user-interface and user-model

--- src/app/moules/users/users.model.ts
+++ src/app/moules/users/users.model.ts
@@ -1,10 +1,25 @@
-// import { Schema, model } from 'mongoose'
-// import { IUser } from './users.interface'
+import { Model, Schema, model } from 'mongoose'
+import { IUser } from './users.interface'
 
-// export const userSchema = new Schema<IUser>({
-//   id: {
-//     type: string,
-//     require: true,
-//     unique: true,
-//   },
-// })
+type UserModel = Model<IUser, object>
+
+export const userSchema = new Schema<IUser>(
+  {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    role: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+)
+
+export const User = model<IUser, UserModel>('User', userSchema)
